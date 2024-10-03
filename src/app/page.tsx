9aa67ed39beb75ac2dd438d378d98803b111389a
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2024-10-03 16:43:07 -0600

    quick link to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
 
   return (
     <div>
       <main>
         <h1>Home page</h1>
+        <Link href="/components">View Components</Link>
       </main>
     </div>
   );
