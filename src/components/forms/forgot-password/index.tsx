885878f00ab6ac183fabb031d2a4e8e1fe51182e
Kyle Holloway <49692633+kylepholloway@users.noreply.github.com>
2025-01-07 17:01:39 -0700

    component naming

--- src/components/forms/forgot-password/index.tsx
+++ src/components/forms/forgot-password/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import Input from '@components/controls/input-temp';
+import Input from '@components/controls/input';
 import Button from '@components/controls/button';
 import { forgotPassword } from '@services/auth';
 
